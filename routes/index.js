3037639e3e26a6a732c69e52ca94fbc88a0d5ab7
Bruno Rocha Mercado <bruno.rocha@bg.com.bo>
2021-12-17 13:41:05 -0400

    Prueba de pull request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('<p>HTML Data</p>');
+  res.send('<p>HTML Datas</p>');
 });
 
 module.exports = router;
